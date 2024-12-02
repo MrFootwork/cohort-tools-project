71a10c54ff27871fc65f8b4fed4ea45bc8c6ac4e
Pandau <pandau.ting@gmail.com>
2024-12-02 16:04:56 +0100

    add cors handling

--- server/app.js
+++ server/app.js
@@ -1,36 +1,41 @@
-const express = require("express");
-const morgan = require("morgan");
-const cookieParser = require("cookie-parser");
+const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
 const PORT = 5005;
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
 
-
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
 
-
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
-// ...
+// Use the CORS middleware with options to allow requests
+// from specific IP addresses and domains.
+app.use(
+	cors({
+		// Add the URLs of allowed origins to this array
+		origin: ['http://localhost:5173', 'http://example.com'],
+	})
+);
 app.use(express.json());
-app.use(morgan("dev"));
-app.use(express.static("public"));
+app.use(morgan('dev'));
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
 
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
 // ...
-app.get("/docs", (req, res) => {
-  res.sendFile(__dirname + "/views/docs.html");
+app.get('/docs', (req, res) => {
+	res.sendFile(__dirname + '/views/docs.html');
 });
-
 
 // START SERVER
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+	console.log(`Server listening on port ${PORT}`);
 });
